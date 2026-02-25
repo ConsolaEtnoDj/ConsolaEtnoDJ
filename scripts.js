e1241b4cbcc7c35f2b4733bd13de72c4c981b806
Juan Sebastian Mendez <proyecto.etnodj@gmail.com>
2026-02-25 11:49:20 +0100

    Revert "Inclusión de  diseño de los ajustes razonables"
    This reverts commit 276036e95af1644e6aee2b43f8b76ce82f65bcfe.

--- scripts.js
+++ scripts.js
@@ -472,7 +472,6 @@ document.addEventListener('DOMContentLoaded', function() {
             grabando = false;
             audiosUtilizadosEnGrabacion.clear();
             botonGrabar.classList.remove('activo');
-            botonDetener.classList.add('desactivado');
             botonDescargar.disabled = true;
 
             // Limpia y muestra placeholder directamente al apagar
@@ -493,13 +492,6 @@ document.addEventListener('DOMContentLoaded', function() {
             visualizador.appendChild(placeholder);
         }
         botonDetener.querySelector('i').className = `fa-solid ${consolaEncendida && !enPausa ? 'fa-pause' : 'fa-play'}`;
-
-        if (botonDetener.querySelector('i').className  === 'fa-solid fa-pause' ) {
-            botonDetener.classList.add('desactivado'); // Agrega la clase para el fondo
-        } else {
-            botonDetener.classList.remove('desactivado'); // Elimina la clase si no está en pausa
-        }
-        
         actualizarBotonesDeAudios();
         actualizarVisualizador();
     });
@@ -517,11 +509,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
         botonDetener.querySelector('i').className = `fa-solid ${enPausa ? 'fa-play' : 'fa-pause'}`;
-        if (botonDetener.querySelector('i').className  === 'fa-solid fa-pause' ) {
-            botonDetener.classList.add('desactivado'); // Agrega la clase para el fondo
-        } else {
-            botonDetener.classList.remove('desactivado'); // Elimina la clase si no está en pausa
-        }
         actualizarBotonesDeAudios();
     });
 
